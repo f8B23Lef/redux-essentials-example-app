17833559b13c8628cdc47a8dac8d4302d1a63f76
Iryna Malash <mal7rish@gmail.com>
2023-05-10 21:17:58 +0500

    Sort posts and add post reactions

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom';
 
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
+import { ReactionButtons } from './ReactionButtons';
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts);
 
-  const renderedPosts = posts.map(post => (
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <PostAuthor userId={post.userId} />
       <TimeAgo timestamp={post.date} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
