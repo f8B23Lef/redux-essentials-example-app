d0a2a793546c754931ba659488c40a4e7e15a8b1
Iryna Malash <mal7rish@gmail.com>
2023-05-14 19:17:15 +0500

    Convert EditPostForm to use RTK mutation
    Invalidate individual Posts by ID to refetch on edit

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -1,26 +1,25 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
-import { postUpdated, selectPostById } from './postsSlice'
+import { useGetPostQuery, useEditPostMutation } from '../api/apiSlice'
 
 export const EditPostForm = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector(state => selectPostById(state, postId))
+  const { data: post } = useGetPostQuery(postId)
+  const [updatePost, { isLoading }] = useEditPostMutation()
 
   const [title, setTitle] = useState(post.title)
   const [content, setContent] = useState(post.content)
 
-  const dispatch = useDispatch()
   const history = useHistory()
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
 
-  const onSavePostClicked = () => {
+  const onSavePostClicked = async () => {
     if (title && content) {
-      dispatch(postUpdated({ id: postId, title, content }))
+      await updatePost({ id: postId, title, content })
       history.push(`/posts/${postId}`)
     }
   }
@@ -37,6 +36,7 @@ export const EditPostForm = ({ match }) => {
           placeholder="What's on your mind?"
           value={title}
           onChange={onTitleChanged}
+          disabled={isLoading}
         />
         <label htmlFor="postContent">Content:</label>
         <textarea
@@ -44,9 +44,10 @@ export const EditPostForm = ({ match }) => {
           name="postContent"
           value={content}
           onChange={onContentChanged}
+          disabled={isLoading}
         />
       </form>
-      <button type="button" onClick={onSavePostClicked}>
+      <button type="button" onClick={onSavePostClicked} disabled={isLoading}>
         Save Post
       </button>
     </section>
