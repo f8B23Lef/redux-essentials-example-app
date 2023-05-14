81c4c18fe1a1166729a1720d821173b36fd8125d
Iryna Malash <mal7rish@gmail.com>
2023-05-14 19:38:57 +0500

    Convert users selectors to read from API cache

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice';
-import userReducer from '../features/users/usersSlice';
 import notificationsReducer from '../features/notifications/notificationsSlice';
 import { apiSlice } from '../features/api/apiSlice';
 
 const store = configureStore({
   reducer: {
     posts: postsReducer,
-    users: userReducer,
     notifications: notificationsReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
